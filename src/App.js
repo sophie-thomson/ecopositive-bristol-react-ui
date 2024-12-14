30d95ff251dc8f7c222ad8b214551e520de24383
Sophie Thomson <sophielbenoit@gmail.com>
2024-12-14 19:40:22 +0000

    Add NavLink and styling to navigation and routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from "react-router-dom";
 
 
 function App() {
@@ -7,7 +9,15 @@ function App() {
     <div className={styles.App}>
         <div className={styles.bg}>
             <NavBar />
-          </div>        
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+                    <Route exact path="/signup" render={() => <h1>Sign Up</h1>} />
+                    <Route exact path="/" render={() => <h1>Directory</h1>} />
+                    <Route render={() => <p>Page not Found!</p>} />
+                </Switch>
+            </Container>
+        </div>        
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
+import { NavLink } from "react-router-dom";
 
 import logo from '../assets/logo.png'
 import styles from "../styles/NavBar.module.css"
@@ -11,25 +12,42 @@ const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" sticky="top">
             <Container>
-                <Navbar.Brand>
-                    <img className='mr-3' src={logo} alt='EcoPositive Logo' height='65' />
-                    ecoPositive    
-                </Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img className='mr-3' src={logo} alt='EcoPositive Logo' height='65' />
+                        ecoPositive    
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
-                    <Nav.Link>
+                    <NavLink
+                        exact
+                        className={styles.NavLink}
+                        activeClassName={styles.Active}
+                        to="/"
+                    >
                         <i className="fa-solid fa-book-open"></i>
                         Browse
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink
+                        // exact
+                        className={styles.NavLink}
+                        activeClassName={styles.Active}
+                        to="/signin"
+                    >
                     <i className="fa-solid fa-arrow-right-to-bracket"></i>
                         Sign In
-                    </Nav.Link>
-                    <Nav.Link>
+                    </NavLink>
+                    <NavLink
+                        // exact
+                        className={styles.NavLink}
+                        activeClassName={styles.Active}
+                        to="/signup"
+                    >
                     <i className="fas fa-user-plus"></i>
                         Sign Up
-                    </Nav.Link>
+                    </NavLink>
                     </Nav>
                     
                 </Navbar.Collapse>
