828740c2624ca0c0ad64b884511893e02be9dc85
Sophie Thomson <sophielbenoit@gmail.com>
2024-12-14 18:21:01 +0000

    Add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className={styles.App}>
+        <div className={styles.bg}>
+            <NavBar />
+          </div>        
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,14 +4,16 @@ import { Nav } from 'react-bootstrap';
 import { Container } from 'react-bootstrap';
 
 import logo from '../assets/logo.png'
+import styles from "../styles/NavBar.module.css"
 
 
 const NavBar = () => {
     return (
-        <Navbar expand="md" sticky="top">
+        <Navbar className={styles.NavBar} expand="md" sticky="top">
             <Container>
                 <Navbar.Brand>
-                    <img src={logo} alt='EcoPositive Logo' height='65' />
+                    <img className='mr-3' src={logo} alt='EcoPositive Logo' height='65' />
+                    ecoPositive    
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
