f8e3cfe77a72fcbe19077bfff39f9e3c86c691cc
Sophie Thomson <sophielbenoit@gmail.com>
2024-12-15 23:03:16 +0000

    Remove unused code

--- src/components/DotsDropdown.js
+++ src/components/DotsDropdown.js
@@ -6,14 +6,14 @@ import { useHistory } from "react-router";
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
-  <i
-    className="fas fa-ellipsis-v"
-    ref={ref}
-    onClick={(e) => {
-      e.preventDefault();
-      onClick(e);
-    }}
-  />
+    <i
+        className="fas fa-ellipsis-v"
+        ref={ref}
+        onClick={(e) => {
+            e.preventDefault();
+            onClick(e);
+        }}
+    />
 ));
 
 export const DotsDropdown = ({ handleEdit, handleDelete }) => {
@@ -43,34 +43,3 @@ export const DotsDropdown = ({ handleEdit, handleDelete }) => {
     </Dropdown>
   );
 };
-
-// export function ProfileEditDropdown({ id }) {
-//   const history = useHistory();
-//   return (
-//     <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
-//       <Dropdown.Toggle as={ThreeDots} />
-//       <Dropdown.Menu>
-//         <Dropdown.Item
-//           onClick={() => history.push(`/profiles/${id}/edit`)}
-//           aria-label="edit-profile"
-//         >
-//           <i className="fas fa-edit" /> edit profile
-//         </Dropdown.Item>
-//         <Dropdown.Item
-//           onClick={() => history.push(`/profiles/${id}/edit/username`)}
-//           aria-label="edit-username"
-//         >
-//           <i className="far fa-id-card" />
-//           change username
-//         </Dropdown.Item>
-//         <Dropdown.Item
-//           onClick={() => history.push(`/profiles/${id}/edit/password`)}
-//           aria-label="edit-password"
-//         >
-//           <i className="fas fa-key" />
-//           change password
-//         </Dropdown.Item>
-//       </Dropdown.Menu>
-//     </Dropdown>
-//   );
-// }
