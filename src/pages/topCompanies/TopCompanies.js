dda06791edba8811ce481d61b59d3499dcb8872f
Sophie Thomson <sophielbenoit@gmail.com>
2025-01-09 22:12:11 +0000

    Remove unusued states and tidy up code

--- src/pages/topCompanies/TopCompanies.js
+++ src/pages/topCompanies/TopCompanies.js
@@ -12,7 +12,6 @@ const TopCompanies = ({mobile}) => {
     const [hasLoaded, setHasLoaded] = useState(false);
     const [topCompanies, setTopCompanies] = useState([]);
     const [topFive, setTopFive] = useState([]);
-    // const [approvedCompanies, setApprovedCompanies] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -44,71 +43,75 @@ const TopCompanies = ({mobile}) => {
         handleMount();
     }, []);
     
-    return (
-        
+    return ( 
         <Container
-        className={`${styles.Border}  ${
-            mobile && "d-lg-none text-center mb-3"
-          }`}
+            className={`${styles.Border}  ${
+                mobile && "d-lg-none text-center mb-3"
+            }`}
         >
-                <>
-                    <p className={`${styles.Header} m-2 pb-2`}>
-                        <span className={styles.Name}>
-                    <i className="fa-solid fa-award" />
+            <>
+                <p className={`${styles.Header} m-2 pb-2`}>
+                    <span className={styles.Name}>
+                        <i className="fa-solid fa-award" />
                         Highly recommended
-                        </span>
-                    </p>
-                    <hr className={`${appStyles.Rule} mt-2`} />
-                    <div>
+                    </span>
+                </p>
+                <hr className={`${appStyles.Rule} mt-2`} />
+                <div>
                     {hasLoaded ? (
-                    
-                <>
-                        {mobile ? (
-                            <Col className="col-flex">
-                            <div className="d-flex justify-content-around flex-wrap flex-md-nowrap">
-                                {topCompanies.map ((company) => (
-                                    <Card key={company.id} className={`${styles.Card} align-items-center m-1`}>
-                                    <TopCompany 
-                                        key={company.id} 
-                                        company={company} 
-                                        name={company.name}
-                                        logo={company.logo}
-                                        mobile
-                                    />
-                                    </Card>
-                                ))}
-                            </div>
-                            </Col>
-                        ) : (
-                            <Col className="col-flex align-items-center">
-                                {topFive.map ((company) => (
-                                    <Card 
-                                        key={company.id} 
-                                        className={`${styles.CardLg} 
-                                            d-flex justify-content-center text-center flex-wrap m-1`
-                                        }
+                        <>
+                            {mobile ? (
+                                <Col className="col-flex">
+                                    <div 
+                                        className="d-flex justify-content-around flex-wrap flex-md-nowrap"
                                     >
-                                        <TopCompany 
-                                            key={company.id}
-                                            company={company} 
-                                            name={company.name}
-                                            logo={company.logo}
-                                        />
-                                    </Card>
-                                ))}
-                            </Col>
-                        )}
+                                        {topCompanies.map ((company) => (
+                                            <Card 
+                                                key={company.id} 
+                                                className={`${styles.Card} align-items-center m-1`}
+                                            >
+                                                <TopCompany 
+                                                    key={company.id} 
+                                                    company={company} 
+                                                    name={company.name}
+                                                    logo={company.logo}
+                                                    mobile
+                                                />
+                                            </Card>
+                                        ))}
+                                    </div>
+                                </Col>
+                            ) : (
+                                <Col className="col-flex align-items-center">
+                                    {topFive.map ((company) => (
+                                        <Card 
+                                            key={company.id} 
+                                            className={
+                                                `${styles.CardLg} 
+                                                d-flex 
+                                                justify-content-center 
+                                                text-center 
+                                                flex-wrap 
+                                                m-1`
+                                            }
+                                        >
+                                            <TopCompany 
+                                                key={company.id}
+                                                company={company} 
+                                                name={company.name}
+                                                logo={company.logo}
+                                            />
+                                        </Card>
+                                    ))}
+                                </Col>
+                            )}
                         </>
                     ) : (
                         <Asset spinner />
                     )}
-                    </div>    
-                </>
-            
-                
-                
-        </Container>
-       
+                </div>    
+            </>        
+        </Container>  
     );
 };
 
