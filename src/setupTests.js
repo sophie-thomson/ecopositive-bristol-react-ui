5cac6457b14b47574c597dcff1ebe7acb83da835
Sophie Thomson <sophielbenoit@gmail.com>
2025-01-12 13:58:43 +0000

    Redact code to solve ESLint warnings

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,8 +6,7 @@ import '@testing-library/jest-dom';
 import {setupServer} from 'msw/node'
 import { handlers } from './mocks/handlers';
 
-
-const server = setupServer(... handlers);
+const server = setupServer(...handlers);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
